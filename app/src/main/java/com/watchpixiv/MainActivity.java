a52ce6c6ac0d2d332c37fce0d460d589f4099d4d
a-lives <xl3130504031@163.com>
2024-06-06 20:19:18 +0800

    optimized and added big image view

--- app/src/main/java/com/watchpixiv/MainActivity.java
+++ app/src/main/java/com/watchpixiv/MainActivity.java
@@ -1,12 +1,16 @@
 package com.watchpixiv;
 
-import android.graphics.BitmapFactory;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
-import android.os.Debug;
 import android.util.Log;
-import android.view.WindowManager;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -16,24 +20,19 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 
-import org.w3c.dom.Text;
-
-import java.lang.annotation.Target;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private String baseUrl = "https://pixiv.re/";
     private String nowIdx = "";
     private int nowPage = 1;
+    private boolean loadedImage = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,61 +52,79 @@ public class MainActivity extends AppCompatActivity {
             _load_image(msgText,imageView);
         });
         buttonLast.setOnClickListener((view) -> {
-            if (Objects.equals(nowIdx, "")){
-                // TODO
-            }
             if (nowPage > 1) {
                 nowPage -= 1;
             }
             _load_image(msgText,imageView);
         });
         buttonNext.setOnClickListener((view) -> {
-            if (Objects.equals(nowIdx, "")){
-                // TODO
-            }
             nowPage += 1;
             _load_image(msgText,imageView);
+        });
+        imageView.setOnClickListener((view)-> {
+            if(loadedImage) {
+                bigImageLoader();
+            }
         });
 
     }
 
     private void _load_image(@NonNull TextView msgText, ImageView imageView){
+        loadedImage = false;
         msgText.setText("loading...");
-        if(nowPage == 1){
-            Glide.with(this)
-                    .load(baseUrl + nowIdx + ".jpg")
-                    .listener(new RequestListener<Drawable>(){
-                        @Override
-                        public boolean onLoadFailed(@Nullable GlideException e, @Nullable Object model, @NonNull com.bumptech.glide.request.target.Target<Drawable> target, boolean isFirstResource) {
-                            msgText.setText("Failed: "+baseUrl + nowIdx + ".jpg");
-                            return false;
-                        }
+        String url = baseUrl + nowIdx + ( nowPage==1 ? "" : "-" + nowPage ) + ".jpg";
+        Glide.with(this)
+                .load(url)
+                .listener(new RequestListener<Drawable>(){
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, @Nullable Object model, @NonNull com.bumptech.glide.request.target.Target<Drawable> target, boolean isFirstResource) {
+                        msgText.setText("Failed: "+url);
+                        return false;
+                    }
 
-                        @Override
-                        public boolean onResourceReady(@NonNull Drawable resource, @NonNull Object model, com.bumptech.glide.request.target.Target<Drawable> target, @NonNull DataSource dataSource, boolean isFirstResource) {
-                            msgText.setText("loaded: "+baseUrl + nowIdx + ".jpg");
-                            return false;
-                        }
-                    })
-                    .into(imageView);
+                    @Override
+                    public boolean onResourceReady(@NonNull Drawable resource, @NonNull Object model, com.bumptech.glide.request.target.Target<Drawable> target, @NonNull DataSource dataSource, boolean isFirstResource) {
+                        msgText.setText("loaded: "+url);
+                        loadedImage = true;
+                        return false;
+                    }
+                })
+                .into(imageView);
+    }
+    private void bigImageLoader(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.big_image_dialog_layout);
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(R.color.transparent);
+        dialog.getWindow().setDimAmount(1f);
 
-        }else{
-            Glide.with(this)
-                    .load(baseUrl + nowIdx + "-" + nowPage + ".jpg")
-                    .listener(new RequestListener<Drawable>(){
-                        @Override
-                        public boolean onLoadFailed(@Nullable GlideException e, @Nullable Object model, @NonNull com.bumptech.glide.request.target.Target<Drawable> target, boolean isFirstResource) {
-                            msgText.setText("Failed: "+baseUrl + nowIdx + "-" + nowPage + ".jpg");
-                            return false;
-                        }
+        ImageView image = dialog.findViewById(R.id.bigImage);
 
-                        @Override
-                        public boolean onResourceReady(@NonNull Drawable resource, @NonNull Object model, com.bumptech.glide.request.target.Target<Drawable> target, @NonNull DataSource dataSource, boolean isFirstResource) {
-                            msgText.setText("loaded: "+baseUrl + nowIdx + "-" + nowPage + ".jpg");
-                            return false;
-                        }
-                    })
-                    .into(imageView);
-        }
+        // 动态调整图像大小
+        Display display = getWindow().getWindowManager().getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        ViewGroup.LayoutParams layoutParams = image.getLayoutParams();
+        layoutParams.width = point.x;
+        layoutParams.height = point.y;
+        image.setLayoutParams(layoutParams);
+
+        image.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // TODO 处理手势
+                return false;
+            }
+        });
+
+        Glide.with(this)
+                .load(baseUrl + nowIdx + ( nowPage==1 ? "" : "-" + nowPage ) + ".jpg")
+                .into(image);
+
+        dialog.show();
+
+        image.setOnClickListener((view)->{
+            Log.d("BigImage","Clicked");
+            dialog.dismiss();
+        });
     }
 }
